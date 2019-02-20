3d775d2127b49910d5fccbc650a577d0ae25d588
rubenkristian <rhyuben.starmoon@gmail.com>
2019-02-21 06:03:40 +0700

    news

---
+++ src/components/ListNews.vue
@@ -0,0 +1,29 @@
+<template>
+    <v-list-tile
+        :key="item.id"
+        avatar
+        ripple
+        @click="loadNews"
+        >
+        <v-list-tile-content>
+            <v-list-tile-title v-html="item.title"></v-list-tile-title>
+            <v-list-tile-sub-title v-html="item.subtitle"></v-list-tile-sub-title>
+        </v-list-tile-content>
+        <v-list-tile-action>
+            <v-list-tile-action-text>{{ item.timestample}}</v-list-tile-action-text>
+        </v-list-tile-action>
+    </v-list-tile>
+</template>
+
+<script>
+export default {
+    props: {
+        item: Object
+    },
+    methods: {
+        loadNews(){
+
+        }
+    },
+}
+</script>
--- src/views/News.vue
+++ src/views/News.vue
@@ -4,7 +4,7 @@
       text-xs-center
       wrap
     >
-    <v-list three-line>
+    <v-list>
       <template v-for="(item, index) in items">
         <v-subheader
           v-if="item.header"
@@ -12,16 +12,10 @@
         >
           {{ item.header }}
         </v-subheader>
-
-        <v-list-tile
+        <ListNews 
           v-else
           :key="item.title"
-        >
-          <v-list-tile-content>
-            <v-list-tile-title v-html="item.title"></v-list-tile-title>
-            <v-list-tile-sub-title v-html="item.subtitle"></v-list-tile-sub-title>
-          </v-list-tile-content>
-        </v-list-tile>
+          :item="item"/>
         <v-divider
           v-if="index + 1 < items.length"
           :key="index"
@@ -33,7 +27,11 @@
 </template>
 
 <script>
+  import ListNews from '../components/ListNews'
   export default {
+    components: {
+      ListNews
+    },
     data () {
       return {
         items: [
@@ -41,47 +39,56 @@
           {
             avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
             title: 'Brunch this weekend?',
-            subtitle: "<span class='text--primary'>Ali Connors</span> &mdash; I'll be in your neighborhood doing errands this weekend. Do you want to hang out?"
+            subtitle: "<span class='text--primary'>Ali Connors</span> &mdash; I'll be in your neighborhood doing errands this weekend. Do you want to hang out?",
+            timestample: "1 minutes"
           },
           {
             avatar: 'https://cdn.vuetifyjs.com/images/lists/2.jpg',
             title: 'Summer BBQ <span class="grey--text text--lighten-1">4</span>',
-            subtitle: "<span class='text--primary'>to Alex, Scott, Jennifer</span> &mdash; Wish I could come, but I'm out of town this weekend."
+            subtitle: "<span class='text--primary'>to Alex, Scott, Jennifer</span> &mdash; Wish I could come, but I'm out of town this weekend.",
+            timestample: "4 minutes"
           },
           {
             avatar: 'https://cdn.vuetifyjs.com/images/lists/3.jpg',
             title: 'Oui oui',
-            subtitle: "<span class='text--primary'>Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?"
+            subtitle: "<span class='text--primary'>Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?",
+            timestample: "10 minutes"
           },
           {
             avatar: 'https://cdn.vuetifyjs.com/images/lists/4.jpg',
             title: 'Birthday gift',
-            subtitle: "<span class='text--primary'>Trevor Hansen</span> &mdash; Have any ideas about what we should get Heidi for her birthday?"
+            subtitle: "<span class='text--primary'>Trevor Hansen</span> &mdash; Have any ideas about what we should get Heidi for her birthday?",
+            timestample: "12 minutes"
           },
           {
             avatar: 'https://cdn.vuetifyjs.com/images/lists/5.jpg',
             title: 'Recipe to try',
-            subtitle: "<span class='text--primary'>Britta Holt</span> &mdash; We should eat this: Grate, Squash, Corn, and tomatillo Tacos."
+            subtitle: "<span class='text--primary'>Britta Holt</span> &mdash; We should eat this: Grate, Squash, Corn, and tomatillo Tacos.",
+            timestample: "30 minutes"
           },
           {
             avatar: 'https://cdn.vuetifyjs.com/images/lists/4.jpg',
             title: 'Birthday gift',
-            subtitle: "<span class='text--primary'>Trevor Hansen</span> &mdash; Have any ideas about what we should get Heidi for her birthday?"
+            subtitle: "<span class='text--primary'>Trevor Hansen</span> &mdash; Have any ideas about what we should get Heidi for her birthday?",
+            timestample: "2 hours"
           },
           {
             avatar: 'https://cdn.vuetifyjs.com/images/lists/5.jpg',
             title: 'Recipe to try',
-            subtitle: "<span class='text--primary'>Britta Holt</span> &mdash; We should eat this: Grate, Squash, Corn, and tomatillo Tacos."
+            subtitle: "<span class='text--primary'>Britta Holt</span> &mdash; We should eat this: Grate, Squash, Corn, and tomatillo Tacos.",
+            timestample: "2 hours"
           },
           {
             avatar: 'https://cdn.vuetifyjs.com/images/lists/4.jpg',
             title: 'Birthday gift',
-            subtitle: "<span class='text--primary'>Trevor Hansen</span> &mdash; Have any ideas about what we should get Heidi for her birthday?"
+            subtitle: "<span class='text--primary'>Trevor Hansen</span> &mdash; Have any ideas about what we should get Heidi for her birthday?",
+            timestample: "3 hours"
           },
           {
             avatar: 'https://cdn.vuetifyjs.com/images/lists/5.jpg',
             title: 'Recipe to try',
-            subtitle: "<span class='text--primary'>Britta Holt</span> &mdash; We should eat this: Grate, Squash, Corn, and tomatillo Tacos."
+            subtitle: "<span class='text--primary'>Britta Holt</span> &mdash; We should eat this: Grate, Squash, Corn, and tomatillo Tacos.",
+            timestample: "4 hours"
           }
         ]
       }
