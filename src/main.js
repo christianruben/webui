7fbe39006da812506ab502c9e0243eab80afda3b
christian <rhyuben.starmoon@gmail.com>
2019-02-20 18:12:18 +0700

    page

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import './plugins/axios'
 import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
-import store from './store'
+import store from './_store'
 
 Vue.config.productionTip = false
 
